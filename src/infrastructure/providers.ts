575040aad14d41bf3e1a26d34f05d67c83b7000c
Mateusz Żupa <yetiasg@gmail.com>
2026-01-22 23:50:15 +0100

    chore: update bindings

--- src/infrastructure/providers.ts
+++ src/infrastructure/providers.ts
@@ -1,4 +1,6 @@
 import { appContainer } from "@kernel/di/app-container";
+import { OrderRepositoryPort } from "@modules/order/application/ports/order-repository.port";
+import { OrderRepositoryAdapter } from "@modules/order/infrastructure/sequelize/repositories/order.repository.adapter";
 import { OrganizationRepositoryPort } from "@modules/organization/application/ports/organization-repository.port";
 import { OrganizationRepositoryAdapter } from "@modules/organization/infrastructure/sequelize/repositories/organization.repository.adapter";
 import { UserRepositoryPort } from "@modules/user/application/ports/user-repository.port";
@@ -7,4 +9,5 @@ import { UserRepositoryAdapter } from "@modules/user/infrastructure/persistence/
 export function registerBindings(): void {
   appContainer.bind(OrganizationRepositoryPort, OrganizationRepositoryAdapter);
   appContainer.bind(UserRepositoryPort, UserRepositoryAdapter);
+  appContainer.bind(OrderRepositoryPort, OrderRepositoryAdapter);
 }
