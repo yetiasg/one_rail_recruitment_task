076d45155cc580f7e0246440d6690fcbdd753910
Mateusz Żupa <yetiasg@gmail.com>
2026-01-22 23:42:58 +0100

    feat: implement OrganizationRepositoryAdapter

--- src/modules/organization/infrastructure/sequelize/repositories/organization.repository.adapter.ts
+++ src/modules/organization/infrastructure/sequelize/repositories/organization.repository.adapter.ts
@@ -1,4 +1,8 @@
-import { OrganizationRepositoryPort } from "@modules/organization/application/ports/organization-repository.port";
+import {
+  FindOrganizationsRepoQuery,
+  FindOrganizationsRepoResult,
+  OrganizationRepositoryPort,
+} from "@modules/organization/application/ports/organization-repository.port";
 import { Organization } from "@modules/organization/domain/entities/organization.entity";
 import { OrganizationModel } from "../models/organization.model";
 import { Injectable } from "@kernel/di/injectable.decorator";
@@ -6,19 +10,43 @@ import { OrganizationPersistenceMapper } from "../mappers/organization.persisten
 
 @Injectable()
 export class OrganizationRepositoryAdapter implements OrganizationRepositoryPort {
+  async findById(id: Organization["id"]): Promise<Organization | null> {
+    const org = await OrganizationModel.findByPk(id);
+    if (!org) return null;
+    return OrganizationPersistenceMapper.toDomain(org);
+  }
+
   async existsById(id: Organization["id"]): Promise<boolean> {
     const count = await OrganizationModel.count({ where: { id } });
     return count > 0;
   }
 
-  async create(organization: Omit<Organization, "id">): Promise<Organization> {
-    const org = await OrganizationModel.create(organization);
+  async findPaged(
+    query: FindOrganizationsRepoQuery,
+  ): Promise<FindOrganizationsRepoResult> {
+    const { rows, count } = await OrganizationModel.findAndCountAll({
+      offset: query.offset,
+      limit: query.limit,
+      order: [[query.orderBy.field, query.orderBy.direction.toUpperCase()]],
+    });
+
+    return {
+      rows: rows.map((row) => OrganizationPersistenceMapper.toDomain(row)),
+      total: count,
+    };
+  }
+
+  async create(data: Omit<Organization, "id">): Promise<Organization> {
+    const org = await OrganizationModel.create(data);
     return OrganizationPersistenceMapper.toDomain(org);
   }
 
-  async findAll(): Promise<Organization[]> {
-    const rows = await OrganizationModel.findAll();
-    return rows.map((row) => OrganizationPersistenceMapper.toDomain(row));
+  async update(data: Organization): Promise<Organization> {
+    const [, org] = await OrganizationModel.update(data, {
+      where: { id: data.id },
+      returning: true,
+    });
+    return OrganizationPersistenceMapper.toDomain(org[0]);
   }
 
   async delete(id: Organization["id"]): Promise<boolean> {
