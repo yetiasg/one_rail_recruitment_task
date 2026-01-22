234cea60195582dbf621646a3a84a779ac470a84
Mateusz Żupa <yetiasg@gmail.com>
2026-01-22 18:18:14 +0100

    feat: implement user model

---
+++ src/modules/user/infrastructure/persistence/sequelize/models/user.model.ts
@@ -0,0 +1,52 @@
+import { sequelize } from "@infrastructure/db/sequelize.instance";
+import { DataTypes, Model } from "sequelize";
+
+export class UserModel extends Model {
+  declare id: string;
+  declare firstName: string;
+  declare lastName: string;
+  declare email: string;
+  declare organizationId: string;
+  declare dateCreated: Date;
+  declare updatedAt: Date;
+}
+
+UserModel.init(
+  {
+    id: {
+      type: DataTypes.STRING(36),
+      primaryKey: true,
+      allowNull: false,
+    },
+    firstName: { type: DataTypes.STRING(100), allowNull: false },
+    lastName: { type: DataTypes.STRING(100), allowNull: false },
+    email: { type: DataTypes.STRING(100), allowNull: false, unique: true },
+    organizationId: {
+      type: DataTypes.STRING(36),
+      allowNull: false,
+      field: "organization_id",
+      references: {
+        model: "organizations",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "RESTRICT",
+    },
+    dateCreated: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: "date_created",
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: "updated_at",
+    },
+  },
+  {
+    sequelize,
+    tableName: "users",
+    timestamps: false,
+    underscored: true,
+  },
+);
