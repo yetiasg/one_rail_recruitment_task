05663f154b889c7c640368764022916e7cad9c1c
Mateusz Żupa <yetiasg@gmail.com>
2026-01-22 23:47:46 +0100

    feat: implement DeleteOrganizationUseCase

---
+++ src/modules/organization/application/use-cases/delete-organization.use-case.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@kernel/di/injectable.decorator";
+import { type OrganizationRepositoryPort } from "../ports/organization-repository.port";
+
+export interface DeleteOrganizationInput {
+  id: string;
+}
+
+@Injectable()
+export class DeleteOrganizationUseCase {
+  constructor(private readonly orgRepo: OrganizationRepositoryPort) {}
+
+  async execute({ id }: DeleteOrganizationInput): Promise<boolean> {
+    const organizationExist = await this.orgRepo.existsById(id);
+    if (!organizationExist) throw new Error(`Organization ${id} not found.`);
+    return this.orgRepo.delete(id);
+  }
+}
