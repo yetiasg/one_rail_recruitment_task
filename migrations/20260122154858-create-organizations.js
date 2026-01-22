b9c0c4a962829605688b405cacd69661bf420bfb
Mateusz Żupa <yetiasg@gmail.com>
2026-01-22 18:15:51 +0100

    feat: create organizations migration

---
+++ migrations/20260122154858-create-organizations.js
@@ -0,0 +1,60 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("organizations", {
+      id: {
+        type: Sequelize.CHAR(36),
+        allowNull: false,
+        primaryKey: true,
+      },
+
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+
+      industry: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+
+      dateFounded: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal(
+          "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP",
+        ),
+      },
+    });
+
+    await queryInterface.sequelize.query(`
+      CREATE TRIGGER organizations_before_insert
+      BEFORE INSERT ON organizations
+      FOR EACH ROW
+      BEGIN
+        IF NEW.id IS NULL OR NEW.id = '' THEN
+          SET NEW.id = UUID();
+        END IF;
+      END;
+    `);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.sequelize.query(
+      `DROP TRIGGER IF EXISTS organizations_before_insert;`,
+    );
+    await queryInterface.dropTable("organizations");
+  },
+};
