238553ed6e151de63d793b6ca790df5221740525
Mateusz Żupa <yetiasg@gmail.com>
2026-01-22 18:15:59 +0100

    feat: create users migration

---
+++ migrations/20260122170410-create-users.js
@@ -0,0 +1,72 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.CHAR(36),
+        allowNull: false,
+        primaryKey: true,
+      },
+
+      firstName: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+
+      lastName: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+
+      organizationId: {
+        type: Sequelize.CHAR(36),
+        allowNull: false,
+        references: {
+          model: "organizations",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "RESTRICT",
+      },
+
+      dateCreated: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal(
+          "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP",
+        ),
+      },
+    });
+
+    await queryInterface.sequelize.query(`
+      CREATE TRIGGER users_before_insert
+      BEFORE INSERT ON users
+      FOR EACH ROW
+      BEGIN
+        IF NEW.id IS NULL OR NEW.id = '' THEN
+          SET NEW.id = UUID();
+        END IF;
+      END;
+    `);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.sequelize.query(
+      `DROP TRIGGER IF EXISTS users_before_insert;`,
+    );
+    await queryInterface.dropTable("users");
+  },
+};
