f3e074e6139eff6865128bd6b51c6c2f028876bc
Mateusz Żupa <yetiasg@gmail.com>
2026-01-22 18:20:45 +0100

    feat: create organization model

---
+++ src/modules/organization/infrastructure/sequelize/models/organization.model.ts
@@ -0,0 +1,41 @@
+import { sequelize } from "@infrastructure/db/sequelize.instance";
+import { DataTypes, Model } from "sequelize";
+
+export class OrganizationModel extends Model {
+  declare id: string;
+  declare name: string;
+  declare industry: string;
+  declare dateFounded: Date;
+  declare createdAt: Date;
+  declare updatedAt: Date;
+}
+
+OrganizationModel.init(
+  {
+    id: {
+      type: DataTypes.STRING(36),
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: { type: DataTypes.STRING(100), allowNull: false },
+    industry: { type: DataTypes.STRING(100), allowNull: false },
+    dateFounded: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "organizations",
+    timestamps: true,
+    underscored: true,
+  },
+);
