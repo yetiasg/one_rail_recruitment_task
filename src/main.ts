b8839f3aa6103549edf1033add037714d1db5372
Mateusz Żupa <yetiasg@gmail.com>
2026-01-23 20:58:13 +0100

    feat: mount health, readiness and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,32 @@
+import "reflect-metadata";
+
 import "./adapters/http";
 import "./infrastructure";
 
-import "reflect-metadata";
 import chalk from "chalk";
-
 import { registerControllers } from "@kernel/runtime/register-controllers";
 import { createApp } from "@adapters/http/app";
-import { registerBindings } from "@infrastructure/providers";
+import { registerBindings } from "@infrastructure/bindings";
 import { globalExceptionFilter } from "@kernel/http/global-exception-filter";
-import { mountSwagger } from "@adapters/http/swagger";
+import { mountSwagger } from "@adapters/http/swagger/swagger";
+import { mountReadiness } from "@adapters/http/readiness/readiness";
+import { sequelize } from "@infrastructure/db/sequelize.instance";
+import { mountHealth } from "@adapters/http/health/health";
 
-const app = createApp();
+function bootstrap() {
+  const app = createApp();
 
-registerBindings();
-registerControllers(app);
-app.use(globalExceptionFilter);
-mountSwagger(app);
+  registerBindings();
+  registerControllers(app);
 
-app.listen(3000, () =>
-  console.log(chalk.blue("Server is litening on port 3000")),
-);
+  app.use(globalExceptionFilter);
+
+  mountReadiness(app, { sequelize });
+  mountHealth(app);
+  mountSwagger(app);
+
+  app.listen(3000, () =>
+    console.log(chalk.blue("Server is litening on port 3000")),
+  );
+}
+void bootstrap();
