4ee25466268adf85b01e192ac61be2ddabf3d9d6
Mateusz Żupa <yetiasg@gmail.com>
2026-01-23 10:30:35 +0100

    chore: update pagination shared types

--- src/modules/order/application/ports/order-repository.port.ts
+++ src/modules/order/application/ports/order-repository.port.ts
@@ -1,4 +1,8 @@
 import { Order } from "@modules/order/domain/entities/order.entity";
+import {
+  FindPagedRepoQuery,
+  FindPagedRepoResult,
+} from "@shared/application/pagination/pagination.type";
 
 export interface OrderWithRelations {
   order: Order;
@@ -28,13 +32,14 @@ export abstract class OrderRepositoryPort {
 
   abstract existsById(id: string): Promise<boolean>;
   abstract findById(id: string): Promise<Order | null>;
-
+  abstract findPaged<F extends string>(
+    query: FindPagedRepoQuery<F>,
+  ): Promise<FindPagedRepoResult<Order>>;
   /**
    * Must ensure userId and organizationId exist (DB FK will enforce).
    * We also typically verify in use-case before calling this.
    */
   abstract create(data: Omit<Order, "id">): Promise<Order>;
-
   abstract update(order: Order): Promise<Order>;
   abstract delete(id: Order["id"]): Promise<boolean>;
 }
