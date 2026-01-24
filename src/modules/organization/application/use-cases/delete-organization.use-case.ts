2b97ba975e6cf7df9363c4b6900208b4a4da62f7
Mateusz Żupa <yetiasg@gmail.com>
2026-01-24 10:53:15 +0100

    fix: implement proper exception instead of raw error

--- src/modules/organization/application/use-cases/delete-organization.use-case.ts
+++ src/modules/organization/application/use-cases/delete-organization.use-case.ts
@@ -1,14 +1,16 @@
 import { Injectable } from "@kernel/di/injectable.decorator";
 import { OrganizationRepositoryPort } from "../ports/organization-repository.port";
 import { Organization } from "@modules/organization/domain/entities/organization.entity";
+import { NotFoundException } from "@kernel/http/http-exceptions";
 
 @Injectable()
 export class DeleteOrganizationUseCase {
   constructor(private readonly orgRepo: OrganizationRepositoryPort) {}
 
-  async execute(id: Organization["id"]): Promise<boolean> {
-    const organizationExist = await this.orgRepo.existsById(id);
-    if (!organizationExist) throw new Error(`Organization ${id} not found.`);
-    return this.orgRepo.delete(id);
+  async execute(id: Organization["id"]): Promise<Organization> {
+    const org = await this.orgRepo.findById(id);
+    if (!org) throw new NotFoundException(`Organization not found.`);
+    await this.orgRepo.delete(id);
+    return org;
   }
 }
