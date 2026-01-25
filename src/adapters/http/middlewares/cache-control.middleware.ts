965576c34bb544ded52814f327f4412c4b7d8468
Mateusz Żupa <yetiasg@gmail.com>
2026-01-26 00:58:34 +0100

    feat: set cache headers for GET users and GET organizations

--- src/adapters/http/middlewares/cache-control.middleware.ts
+++ src/adapters/http/middlewares/cache-control.middleware.ts
@@ -1,17 +1,14 @@
 import type { Request, Response, NextFunction } from "express";
 
-const TEN_MINUTES_SECONDS = 600;
+export function cacheControlGetOnly(matchPath: string, timeInSeconds: number) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const requestPath = req.path ?? req.url;
 
-export function cacheControl10m(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
-  const path = req.path ?? req.url;
+    if (req.method === "GET" && requestPath.startsWith(matchPath)) {
+      // Use "private" if responses may depend on auth/user-specific data
+      res.setHeader("Cache-Control", `private, max-age=${timeInSeconds}`);
+    }
 
-  if (path.startsWith("/api/users") || path.startsWith("/api/organizations")) {
-    res.setHeader("Cache-Control", `public, max-age=${TEN_MINUTES_SECONDS}`);
-  }
-
-  next();
+    next();
+  };
 }
